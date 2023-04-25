98d0b5c81fb98a5f605e34000a42007253949768
Chad Moore <orangetinyterror@gmail.com>
2023-04-25 15:14:56 -0400

    Added blank game screen.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import sys
 
-print("hello world")
 print(f"Python {sys.version}")
 
 import pygame
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode([320, 200])
+    done = False
+    while not done:
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+            elif event.type == pygame.QUIT:
+                done = True
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
