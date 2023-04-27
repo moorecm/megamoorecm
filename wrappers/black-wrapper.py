4d1362b8975a88367cefe19d2fa98aa5506d51d4
Chad Moore <orangetinyterror@gmail.com>
2023-04-27 15:06:32 -0400

    Updated bazel run black to find all Python files.

--- wrappers/black-wrapper.py
+++ wrappers/black-wrapper.py
@@ -1,5 +1,8 @@
 import black
+import os
+import pathlib
 import sys
 
 if __name__ == "__main__":
-    sys.exit(black.main())
+    workspace = pathlib.Path(os.environ["BUILD_WORKSPACE_DIRECTORY"])
+    sys.exit(black.main(["--verbose"] + [str(p) for p in workspace.rglob("*.py")]))
