3866da4b5ef2efedfa511881a3e6490952fdd771
Chad Moore <orangetinyterror@gmail.com>
2023-04-27 15:29:57 -0400

    Skeleton game loop with 30 FPS synchronization.

--- game/engine.py
+++ game/engine.py
@@ -5,6 +5,7 @@ class Engine:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode([320, 200])
+        self.clock = pygame.time.Clock()
 
     def __del__(self):
         pygame.quit()
@@ -12,10 +13,24 @@ class Engine:
     def loop(self):
         done = False
         while not done:
-            pygame.display.flip()
+            #
+            # handle events
+            #
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         done = True
                 elif event.type == pygame.QUIT:
                     done = True
+            #
+            # update state
+            #
+            pass
+            #
+            # render
+            #
+            pygame.display.flip()
+            #
+            # synchronize
+            #
+            self.clock.tick(30)
