8b8b441a758f19d31005fd70b395f4f41dc35f37
Chad Moore <orangetinyterror@gmail.com>
2023-04-28 14:42:24 -0400

    Scaled display x3.

--- game/engine.py
+++ game/engine.py
@@ -6,7 +6,11 @@ import player
 class Engine:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode([320, 200])
+        self.canvas = pygame.Surface((320, 200))
+        self.scale_factor = 3
+        self.screen = pygame.display.set_mode(
+            [320 * self.scale_factor, 200 * self.scale_factor]
+        )
         self.clock = pygame.time.Clock()
         self.players = pygame.sprite.Group()
         self.player = player.Player()
@@ -34,7 +38,10 @@ class Engine:
             #
             # render
             #
-            self.players.draw(self.screen)
+            self.players.draw(self.canvas)
+            pygame.transform.scale_by(
+                self.canvas, self.scale_factor, dest_surface=self.screen
+            )
             pygame.display.flip()
             #
             # synchronize
