8d09bff332b0abd472d1dbcfeaf563ee25ed227b
Chad Moore <orangetinyterror@gmail.com>
2023-04-28 15:40:44 -0400

    Replaced custom scaling with flags=pygame.SCALED.

--- game/engine.py
+++ game/engine.py
@@ -6,11 +6,7 @@ import player
 class Engine:
     def __init__(self):
         pygame.init()
-        self.canvas = pygame.Surface((320, 200))
-        self.scale_factor = 3
-        self.screen = pygame.display.set_mode(
-            [320 * self.scale_factor, 200 * self.scale_factor]
-        )
+        self.screen = pygame.display.set_mode((320, 200), flags=pygame.SCALED)
         self.clock = pygame.time.Clock()
         self.players = pygame.sprite.Group()
         self.player = player.Player()
@@ -40,10 +36,7 @@ class Engine:
             #
             # render
             #
-            self.players.draw(self.canvas)
-            pygame.transform.scale_by(
-                self.canvas, self.scale_factor, dest_surface=self.screen
-            )
+            self.players.draw(self.screen)
             pygame.display.flip()
             #
             # synchronize
