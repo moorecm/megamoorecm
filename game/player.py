6c1a81c776a06bdd1e53a76960a5f72d2a79a494
Chad Moore <orangetinyterror@gmail.com>
2023-04-28 15:13:00 -0400

    Added periodic blinking.

--- game/player.py
+++ game/player.py
@@ -1,24 +1,49 @@
 import pygame
 
 import json
+import time
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
+        # load the spritesheet
         self.spritesheet = pygame.image.load(
             "external/8bitmegaman/file/8bitmegaman.png"
         ).convert()
+        # load the spritesheet metadata
         with open("game/8bitmegaman.json") as f:
             self.spritemodel = json.load(f)
-        self.image = self.spritesheet.subsurface(
-            pygame.Rect(
-                self.spritemodel["ready"]["x"],
-                self.spritemodel["ready"]["y"],
-                self.spritemodel["ready"]["w"],
-                self.spritemodel["ready"]["h"],
+
+        # prepare images
+        self.images = []
+        for pose in self.spritemodel:
+            self.images.append(
+                self.spritesheet.subsurface(
+                    pygame.Rect(
+                        self.spritemodel[pose]["x"],
+                        self.spritemodel[pose]["y"],
+                        self.spritemodel[pose]["w"],
+                        self.spritemodel[pose]["h"],
+                    )
+                )
             )
-        )
+
+        self.frame = 0
+        self.image = self.images[self.frame]
 
         self.rect = pygame.Rect(160 - 16, 100 - 16, 32, 32)
+
+        self.time = time.time()
+
+    def update(self):
+        # blink periodically
+        per_second_interval = 8
+        count = 24
+        counter = int((time.time() - self.time) * per_second_interval % count)
+        if counter == 8:
+            self.frame = 1
+        else:
+            self.frame = 0
+        self.image = self.images[self.frame]
