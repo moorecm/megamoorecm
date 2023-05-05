3a3517d26ca1072a64e6eb7a50d308d6ee3fe78e
Chad Moore <orangetinyterror@gmail.com>
2023-05-05 00:38:21 -0400

    Fixed tilemap.
    In Tiled, tilesets can be created in 2 ways:  by reading tiles
    from a single image or by reading each tile from separate images.
    
    Since the smb.png tileset has incompatible margins, this change
    updates the map and tileset to use the 2nd method.
    
    Each tile that is intended for collision detection must be assigned
    to class "solid".
    
    Note that the file path in .tmx files may need to be manually
    altered (in order to support Bazel's layout and use in 2 different
    targets) after saving changes in Tiled.

--- game/level.py
+++ game/level.py
@@ -9,27 +9,46 @@ import mapset
 class Level1:
     def __init__(self, group):
         self.group = group
+        width = 16
+        height = 16
         self.map = pytmx.util_pygame.load_pygame("game/level1.tmx")
         for n, layer in enumerate(self.map.layers):
             for x, y, tile in layer.tiles():
+                # x, y are tile indicies, not coordinates
                 properties = self.map.get_tile_properties(x, y, n)
                 if properties is not None:
-                    width = properties.get("width")
-                    height = properties.get("height")
+                    x_offset = properties.get("x")  # x offset into tileset image
+                    y_offset = properties.get("y")  # y offset into tileset image
+                    # properties.get("width") is the tileset image width
+                    # properties.get("height") is the tileset image height
+
+                    # pytmx ignores the .tmx attributes and loads the whole tileset
+                    # image into tile, so this clips it for each single tile.
+                    tile = tile.subsurface(
+                        pygame.Rect(x_offset, y_offset, width, height)
+                    )
+
+                    # add "solid" sprites to the collision detection group
                     if properties.get("type") == "solid":
                         self.group.add(
                             mapset.Solid(
                                 image=tile,
-                                rect=pygame.Rect(x * 16, y * 16, width, height),
+                                rect=pygame.Rect(x * width, y * height, width, height),
                             )
                         )
 
     def draw(self, surface):
+        width = 16
+        height = 16
         for n, layer in enumerate(self.map.layers):
             for x, y, tile in layer.tiles():
                 properties = self.map.get_tile_properties(x, y, n)
-                # if properties is not None:
-                #    width = properties.get("width")
-                #    height = properties.get("height")
-                #    surface.blit(tile, (x * width, y * height))
-                surface.blit(tile, (x * 16, y * 16))
+                if properties is not None:
+                    x_offset = properties.get("x")
+                    y_offset = properties.get("y")
+                    tile = tile.subsurface(
+                        pygame.Rect(x_offset, y_offset, width, height)
+                    )
+                    surface.blit(
+                        tile, pygame.Rect(x * width, y * height, width, height)
+                    )
--- game/smb.tsx
+++ game/smb.tsx
@@ -1,6 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="smb" tilewidth="16" tileheight="16" tilecount="1974" columns="42">
- <image source="../external/smb/file/smb.png" width="680" height="764"/>
- <tile id="42" type="solid"/>
- <tile id="84" type="solid"/>
+<tileset version="1.10" tiledversion="1.10.1" name="smb" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" x="0" y="16" width="16" height="16" type="solid">
+  <image width="680" height="764" source="../external/smb/file/smb.png"/>
+ </tile>
+ <tile id="1" x="0" y="33" width="16" height="16" type="solid">
+  <image width="680" height="764" source="../external/smb/file/smb.png"/>
+ </tile>
+ <tile id="2" x="17" y="33" width="16" height="16">
+  <image width="680" height="764" source="../external/smb/file/smb.png"/>
+ </tile>
 </tileset>
